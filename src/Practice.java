d68ff90dbf8070abbe72f345d96aac74b65dc35b
Galatear <171226854+LadyGalatear@users.noreply.github.com>
2026-04-28 11:52:42 -0700

    Add constructor, add while loop

--- src/Practice.java
+++ src/Practice.java
@@ -1,17 +1,29 @@
 public class Practice {
     public static void main(String[] args) {
-        Node myNode = new Node();
-        myNode.value = 'w';
+        Node myNode = new Node('w', null);
 
-        Node otherNode = new Node();
-        otherNode.value = 'e';
+        Node otherNode = new Node('x', null);
 
-        Node badassNode = otherNode;
+        Node badassNode = new Node('e', null);
 
-        badassNode.value = 't';
+        Node current = myNode;
 
+        myNode.next = otherNode;
+        otherNode.next = badassNode;
+
+        while (current != null) {
+            System.out.println(current.value);
+            current = current.next;
+        }
+
+        /* 
         System.out.println(myNode.value);
         System.out.println(otherNode.value);
         System.out.println(badassNode.value);
+
+        System.out.println(myNode.value);
+        System.out.println(myNode.next.value);
+        System.out.println(myNode.next.next.value);
+        */
     }
 }
